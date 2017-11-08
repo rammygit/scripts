0aa5ee9962d762eed2d81ac5460f61f15c654ea0
ramkumar sundarajan meenakshi <ramkumar.sundarajan.meenakshi@oracle.com>
2017-11-07 22:17:17 -0500

    updated to add the output title format into a directory

--- ydl.sh
+++ ydl.sh
@@ -1,2 +1,2 @@
- 
-youtube-dl -i --embed-thumbnail --extract-audio --audio-format mp3 "$1"
+# updated with output format.  
+youtube-dl -i --embed-thumbnail --extract-audio --audio-format mp3 -o '%(playlist)s/%(title)s.%(ext)s' "$1"
